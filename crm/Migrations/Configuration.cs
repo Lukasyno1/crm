2173a510b080c294ec71a224236b6f1a02895c6c
Łukasz Lula <l.lula@luca.eu>
2020-08-19 18:12:17 +0200

    Create empty crm database

---
+++ crm/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace crm.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<crm.Models.CrmDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(crm.Models.CrmDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ crm/Models/CrmDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace crm.Models
+{
+    public class CrmDbContext : DbContext
+    {
+        public CrmDbContext()
+           : base("name=CrmDbContext")
+        {          
+        }
+
+    }
+}
